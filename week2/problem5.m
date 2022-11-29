a7231657db4c8ca1a4dce35891af95ccf4276fbd
Bjarmi99 <70704830+Bjarmi99@users.noreply.github.com>
2022-11-29 14:03:16 +0000

    pendulum corrected

--- week2/problem5.m
+++ week2/problem5.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-%% Problem 5.1
+% Problem 5.1
 x=[pi/12;0]; %Initial conditions
 T=20;
 n=500;
@@ -9,7 +9,7 @@ L = 2;
 [t, y] = RK_method(x,n,T);
 
 % plot_theta(t,y)
-% figure
+%figure
 plot_pendulum(y,L)
 
 %% Problem 5.2
