e96bbf6b30e84a694f50f110669ab5b4093f38c6
Bjarmi99 <70704830+Bjarmi99@users.noreply.github.com>
2022-12-02 14:11:25 +0000

    PROB 12 MODIFIED

--- week2/animated_curve.m
+++ week2/animated_curve.m
@@ -6,7 +6,6 @@ axis([min(y(1,:)) max(y(1,:)) min(y(3,:)) max(y(3,:))]*1.1) %to get allways the 
 
 for i = 1:length(y(1,:))
     addpoints(path,y(1,i),y(3,i));
-    %addpoints(theta2,t(i),y(3,i));
     %pause(0.05)
     drawnow
     
--- week2/problem10.m
+++ week2/problem10.m
@@ -4,11 +4,12 @@ close all; clear all;clc;
 L1=2; L2=2; m1=1; m2=1;T=50;
 
 y0 = [pi/3;0;pi/6;0];
-n = 10000;
+n =5000;
 
 
 [t y] = RK_method_double_pendulum(y0,n,T,L1,L2,m1,m2);
 animated_curve(y)
+
 
 %We can see that it is filling this rectangle it is nearly always going the
 %same path but nearly neaver in exactly his old paths or in the end he nearly does go exactly in his path.... 
@@ -18,6 +19,7 @@ y0 = [pi/2;0;pi/2;0];
 
 [t y] = RK_method_double_pendulum(y0,n,T,L1,L2,m1,m2);
 animated_curve(y)
+
 
 %% 10.2
 y0 = [pi/2;0;pi/6;0]; 
