42607fef5225886c2f4509d77248e20c51a8d563
eric-ru <eric22@ru.is>
2022-12-02 13:26:06 +0000

    Changed the plot in 9

--- week2/problem9.m
+++ week2/problem9.m
@@ -11,21 +11,21 @@ T = 20;
 [error,n_vec] = RK_calc_errors(y0, n, T, L1, L2, m1, m2);
 
 % Do best fit on errors on loglog plot
-coefficients = polyfit(log(n_vec),log(error),1);
-y1 = polyval(coefficients,log(n_vec));
+%coefficients = polyfit(log(n_vec),log(error),1);
+%y1 = polyval(coefficients,log(n_vec));
 
-plot(log(n_vec),log(error),'*')
-hold on
-plot(log(n_vec),y1)
-xl = xlim;
-yl = ylim;
-xt = 0.7 * (xl(2)-xl(1)) + xl(1);
-yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
-ylabel('log(Error)')
-xlabel('log(n)')
-legend('Errors','Fitted line')
+% plot(log(n_vec),log(error),'*')
+% hold on
+% plot(log(n_vec),y1)
+% xl = xlim;
+% yl = ylim;
+% xt = 0.7 * (xl(2)-xl(1)) + xl(1);
+% yt = 0.75 * (yl(2)-yl(1)) + yl(1);
+% caption_0 = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
+% %text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+% ylabel('log(Error)')
+% xlabel('log(n)')
+% legend('Errors','Fitted line')
 
 
 %% 9.1
@@ -40,15 +40,15 @@ y0 = [pi/2;0;pi/2;0];
 coefficients = polyfit(log(n_vec),log(error),1);
 y1 = polyval(coefficients,log(n_vec));
 
-plot(log(n_vec),log(error),'*')
+plot(log(n_vec), log(error), "r*")
 hold on
-plot(log(n_vec),y1)
+plot(log(n_vec),y1,'r')
 xl = xlim;
 yl = ylim;
 xt = 0.7 * (xl(2)-xl(1)) + xl(1);
 yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+caption_1 = sprintf('y_1 = %.2f * x + %.2f', coefficients(1), coefficients(2));
+%text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
 ylabel('log(Error)')
 xlabel('log(n)')
 legend('Errors','Fitted line')
@@ -65,18 +65,18 @@ y0 = [pi/2;0;pi/6;0];
 coefficients = polyfit(log(n_vec),log(error),1);
 y1 = polyval(coefficients,log(n_vec));
 
-plot(log(n_vec),log(error),'*')
+plot(log(n_vec),log(error),"g*")
 hold on
-plot(log(n_vec),y1)
+plot(log(n_vec),y1,'g')
 xl = xlim;
 yl = ylim;
 xt = 0.7 * (xl(2)-xl(1)) + xl(1);
 yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+caption_2 = sprintf('y_2 = %.2f * x + %.2f', coefficients(1), coefficients(2));
+%text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
 ylabel('log(Error)')
 xlabel('log(n)')
-legend('Errors','Fitted line')
+%legend('Errors','Fitted line')
 
 %% 9.3
 
@@ -88,18 +88,18 @@ y0 = [pi/1.5;0;pi/6;0];
 coefficients = polyfit(log(n_vec),log(error),1);
 y1 = polyval(coefficients,log(n_vec));
 
-plot(log(n_vec),log(error),'*')
+plot(log(n_vec),log(error),'b*')
 hold on
-plot(log(n_vec),y1)
+plot(log(n_vec),y1,'b')
 xl = xlim;
 yl = ylim;
 xt = 0.7 * (xl(2)-xl(1)) + xl(1);
 yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+caption_3 = sprintf('y_3 = %.2f * x + %.2f', coefficients(1), coefficients(2));
+%text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
 ylabel('log(Error)')
 xlabel('log(n)')
-legend('Errors','Fitted line')
+%legend('Errors','Fitted line')
 
 %% 9.4
 
@@ -111,18 +111,18 @@ y0 = [pi/3;0;pi/4;0];
 coefficients = polyfit(log(n_vec),log(error),1);
 y1 = polyval(coefficients,log(n_vec));
 
-plot(log(n_vec),log(error),'*')
+plot(log(n_vec),log(error),"m*")
 hold on
-plot(log(n_vec),y1)
+plot(log(n_vec),y1,'m')
 xl = xlim;
 yl = ylim;
 xt = 0.7 * (xl(2)-xl(1)) + xl(1);
 yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+caption_4 = sprintf('y_4 = %.2f * x + %.2f', coefficients(1), coefficients(2));
+%text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
 ylabel('log(Error)')
 xlabel('log(n)')
-legend('Errors','Fitted line')
+%legend('Errors','Fitted line', caption_4)
 %% 9.5 (like part 5 in 8)
 
 y0 = [pi/3;0;pi;0];
@@ -133,38 +133,38 @@ y0 = [pi/3;0;pi;0];
 coefficients = polyfit(log(n_vec),log(error),1);
 y1 = polyval(coefficients,log(n_vec));
 
-plot(log(n_vec),log(error),'*')
+plot(log(n_vec),log(error),"c*")
 hold on
-plot(log(n_vec),y1)
+plot(log(n_vec),y1,"c")
 xl = xlim;
 yl = ylim;
 xt = 0.7 * (xl(2)-xl(1)) + xl(1);
 yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+caption_5 = sprintf('y_5 = %.2f * x + %.2f', coefficients(1), coefficients(2));
+%text(xt, yt, caption_5, 'FontSize', 9, 'Color', 'k');%
 ylabel('log(Error)')
 xlabel('log(n)')
-legend('Errors','Fitted line')
+%legend('Errors','Fitted line',caption_5)
 
 %% 9.6
 
-y0 = [pi;0;pi;0];
-
-[error,n_vec] = RK_calc_errors(y0, n, T, L1, L2, m1, m2);
-
-% Do best fit on errors on loglog plot
-coefficients = polyfit(log(n_vec),log(error),1);
-y1 = polyval(coefficients,log(n_vec));
-
-plot(log(n_vec),log(error),'*')
-hold on
-plot(log(n_vec),y1)
+% y0 = [pi;0;pi;0];
+% 
+% [error,n_vec] = RK_calc_errors(y0, n, T, L1, L2, m1, m2);
+% 
+% % Do best fit on errors on loglog plot
+% coefficients = polyfit(log(n_vec),log(error),1);
+% y1 = polyval(coefficients,log(n_vec));
+% 
+% plot(log(n_vec),log(error),'*')
+% hold on
+% plot(log(n_vec),y1)
 xl = xlim;
 yl = ylim;
 xt = 0.7 * (xl(2)-xl(1)) + xl(1);
 yt = 0.75 * (yl(2)-yl(1)) + yl(1);
-caption = sprintf('y = %.2f * x + %.2f', coefficients(1), coefficients(2));
-text(xt, yt, caption, 'FontSize', 9, 'Color', 'k');
+%caption_6 = sprintf('y_6 = %.2f * x + %.2f', coefficients(1), coefficients(2));
+%text(xt, yt, caption, 'FontSize', 9, 'Color', 'k'); %caption,
 ylabel('log(Error)')
 xlabel('log(n)')
-legend('Errors','Fitted line')
+legend('Error 1', caption_1, 'Error 2', caption_2, 'Error 3', caption_3, 'Error 4', caption_4, 'Error 5', caption_5)
