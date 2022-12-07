d1b4678c1f60f277b2bd2eb796f80e848ba1fae2
Bjarmi99 <70704830+Bjarmi99@users.noreply.github.com>
2022-12-07 14:00:56 +0000

    name changed on file

--- week3/Project 3/prob3.m
+++ week3/Project 3/prob3.m
@@ -5,13 +5,8 @@ n = 10;
 [A,b] = set_up_matrix(m,n);
 
 v = (A\b)+20;
-v_matrix = reshape(v(1:m*n),m,n);
+v_matrix = reshape(v(1:m*n),m,n); %here we reshape from vector to matrix so it maches the X,Y boundaries
 
-
-% plot(v)
-
-% x = 2:m-1;
-% y = 1:n
 [X,Y] = meshgrid(1:m,1:n);
 
 
