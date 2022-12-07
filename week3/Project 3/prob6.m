a04b47b63dbdbcb491486e8fcb352ffbdf4ffa2a
Bjarmi99 <70704830+Bjarmi99@users.noreply.github.com>
2022-12-07 17:23:19 +0000

    asf

--- week3/Project 3/prob6.m
+++ week3/Project 3/prob6.m
@@ -1,20 +1,17 @@
+%% Problem 6
 close all; clear all; clc;
+
 m = 50;
 n = 20; %values from Problem 4
-L = 2; %cm
 Lx = 4;
 Ly = 4;
- 
+
+L = 2; %cm
+% for i = 0:0.1:4
+%  
 [A,b] = set_up_matrix5(m,n,L,Lx,Ly);
 
 v = (A\b)+20;
 v_matrix = reshape(v(1:m*n),m,n); %here we reshape from vector to matrix so it maches the X,Y boundaries
 v_matrix = v_matrix';
-[X,Y] = meshgrid(1:m,1:n);
 
-
-mesh(X,Y,v_matrix)
-colorbar
-
-highest_temp = v_matrix(1,1);
-fprintf('\nThe highest temperature is: %.4f',highest_temp)
