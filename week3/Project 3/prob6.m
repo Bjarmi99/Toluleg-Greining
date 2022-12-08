d2a7e43082d2dfcf8b6a912a1ca3e191b0eef128
Bjarmi99 <70704830+Bjarmi99@users.noreply.github.com>
2022-12-08 12:34:50 +0000

    6 ready minor fixes needed

--- week3/Project 3/prob6.m
+++ week3/Project 3/prob6.m
@@ -5,6 +5,7 @@ m = 50;
 n = 20; %values from Problem 4
 Lx = 4;
 Ly = 4;
+<<<<<<< Updated upstream
 Ly1 = 0;
 k = Ly/(n-1);
 L = 2; 
@@ -33,4 +34,27 @@ end
 % v = (A\b)+20;
 % v_matrix = reshape(v(1:m*n),m,n); %here we reshape from vector to matrix so it maches the X,Y boundaries
 % v_matrix = v_matrix';
+=======
+L = 2;
 
+%L = 0; %cm
+counter = 1;
+for i = 0:10
+    [A,b] = test(m,n,L,Lx,Ly,i);
+    v = (A\b)+20;
+    v_matrix = reshape(v(1:m*n),m,n); %here we reshape from vector to matrix so it maches the X,Y boundaries
+    v_matrix = v_matrix';
+    v_highest(counter,1) = max(v);
+    v_highest(counter,2) = i;
+    
+    figure
+    [X,Y] = meshgrid(1:m,1:n);
+    mesh(X,Y,v_matrix)
+    colorbar
+    view(2)
+    counter = counter + 1;
+end
+>>>>>>> Stashed changes
+
+figure
+plot(0:10,v_highest(:,1))
